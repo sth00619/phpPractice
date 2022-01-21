bd1cf6b53cd5250e78fd08d3eada90e21fb15ba5
SeoultechUni_Songtaeho <97080735+sth00619@users.noreply.github.com>
2022-01-21 19:24:43 +0900

    Update practice1.php

--- practice1.php
+++ practice1.php
@@ -3,9 +3,22 @@
 $titles=array('제목1','제목2','제목3','제목4','제목5');
 $names=array('글쓴이1','글쓴이2','글쓴이3','글쓴이4','글쓴이5');
 
+
+function cut($str, $len){
+
+  if(strlen($str) > $len) $str=substr($str,0,$len)."...";
+
+  else $str=$str;
+
+  return $str;
+}
+
+
 $tr="";
 for($i=0;$i<5;$i++){
   $n=$i+1;
+
+  $names[$i]=cut($names[$i], 9);
 
   $tr=$tr."<tr>
         <td class='td1'>$n</td>
